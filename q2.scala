386229ecd392fc2fb645ee357cc748243a580e6f
nive19s <sothilingamnivethan19@gmail.com>
2024-08-12 18:35:51 +0530

    q2 added

--- q2.scala
+++ q2.scala
@@ -1,11 +1,13 @@
-object NumberCategorizer extends App {
-  // Check if a number is a multiple of 3
+import scala.io.StdIn.readInt
+
+object NumberCategorizer {
+  // Lambda function to check if a number is a multiple of 3
   val isMultipleOfThree: Int => Boolean = (n: Int) => n % 3 == 0
 
-  // Check if a number is a multiple of 5
+  // Lambda function to check if a number is a multiple of 5
   val isMultipleOfFive: Int => Boolean = (n: Int) => n % 5 == 0
 
-  // Pattern match the input and categorize it
+  // Function to categorize the number using pattern matching
   def categorizeNumber(n: Int): String = (isMultipleOfThree(n), isMultipleOfFive(n)) match {
     case (true, true) => "Multiple of Both Three and Five"
     case (true, false) => "Multiple of Three"
@@ -13,11 +15,9 @@ object NumberCategorizer extends App {
     case _ => "Not a Multiple of Three or Five"
   }
 
-  // Parse the command line input
-  if (args.length > 0) {
-    val input = args(0).toInt
-    println(categorizeNumber(input))
-  } else {
-    println("Please provide an integer input.")
+  def main(args: Array[String]): Unit = {
+    println("Please enter an integer:")
+    val input = readInt()  // Get user input
+    println(categorizeNumber(input))  // Categorize and print the result
   }
 }
