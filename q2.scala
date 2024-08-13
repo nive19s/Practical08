89fc732b6cd46ab481e0e6f49054c15ec68cef0b
nive19s <sothilingamnivethan19@gmail.com>
2024-08-13 11:28:21 +0530

    q2 added

--- q2.scala
+++ q2.scala
@@ -8,7 +8,7 @@ object NumberCategorizer {
     case (true, true) => "Multiple of Both Three and Five"
     case (true, false) => "Multiple of Three"
     case (false, true) => "Multiple of Five"
-    case _ => "Multiple of Three or Five"
+    case _ => "Not Multiple of Three or Five"
   }
 
   def main(args: Array[String]): Unit = {
